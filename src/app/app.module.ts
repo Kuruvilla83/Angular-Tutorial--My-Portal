49c122505ce8a743e590e8e7cf02a73d14914f4e
Mathew <kuruvilla.mathew@accenture.com>
2020-05-01 12:49:52 -0400

    modified:   src/app/app.component.css 	modified:   src/app/app.component.html 	modified:   src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyComponentTestComponent } from './my-component-test/my-component-test.component';
+import { MyComponentContactusComponent } from './my-component-contactus/my-component-contactus.component';
+import { MyComponentAboutmeComponent } from './my-component-aboutme/my-component-aboutme.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponentTestComponent,
+    MyComponentContactusComponent,
+    MyComponentAboutmeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
